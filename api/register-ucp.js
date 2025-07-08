469120b48e7c22fde4aeee9ba7c2c2232a4ddc86
PannYow <pannyow5@gmail.com>
2025-07-08 16:30:51 +0800

    Create register-ucp.js

---
+++ api/register-ucp.js
@@ -0,0 +1,10 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import mysql from 'mysql2/promise';
+import axios from 'axios';
+
+export default async function handler(req, res) {
+    // ... (Kode lengkap akan saya berikan di langkah selanjutnya setelah setup Vercel)
+    // Untuk sekarang, kita buat placeholder dulu
+    res.status(200).send("API Register is working. Code will be added later.");
+}
