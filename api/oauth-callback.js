c21381957b42b8baba661c06e46c8256e82390c8
PannYow <pannyow5@gmail.com>
2025-07-08 16:30:31 +0800

    Create oauth-callback.js

---
+++ api/oauth-callback.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+import jwt from 'jsonwebtoken';
+import mysql from 'mysql2/promise';
+
+export default async function handler(req, res) {
+    // ... (Kode lengkap akan saya berikan di langkah selanjutnya setelah setup Vercel)
+    // Untuk sekarang, kita buat placeholder dulu
+    res.status(200).send("API Callback is working. Code will be added later.");
+}
